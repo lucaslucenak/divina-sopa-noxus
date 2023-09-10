90962a177e46dbe041a36c3283412774905a39de
Lucas Lucena <lucas.lucenak@gmail.com>
2023-09-10 16:01:12 -0300

    Add jpaauditing

--- src/main/java/com/lucalucenak/Noxus/NoxusApplication.java
+++ src/main/java/com/lucalucenak/Noxus/NoxusApplication.java
@@ -2,8 +2,10 @@ package com.lucalucenak.Noxus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NoxusApplication {
 
 	public static void main(String[] args) {
