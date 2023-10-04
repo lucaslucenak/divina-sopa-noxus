fbcc8f37e71186bb47a8bb7bc62caa81b98352d1
Lucas Lucena <lucas.lucenak@gmail.com>
2023-10-04 14:08:48 -0300

    Update SizeServiceTest

--- src/test/java/com/lucalucenak/Noxus/services/SizeServiceTest.java
+++ src/test/java/com/lucalucenak/Noxus/services/SizeServiceTest.java
@@ -2,6 +2,7 @@ package com.lucalucenak.Noxus.services;
 
 import com.lucalucenak.Noxus.dtos.SizeFullDto;
 import com.lucalucenak.Noxus.dtos.post.SizePostDto;
+import com.lucalucenak.Noxus.exceptions.IncompatibleIdsException;
 import com.lucalucenak.Noxus.exceptions.ResourceNotFoundException;
 import com.lucalucenak.Noxus.models.SizeModel;
 import com.lucalucenak.Noxus.repositories.SizeRepository;
@@ -89,8 +90,8 @@ public class SizeServiceTest {
     }
 
     @Test
-    public void SizeService_saveSize_ReturnsSizeFullDtoSaved() {
-
+    public void SizeService_SaveSize_ReturnsSizeFullDtoSaved() {
+        // Arrange
         // Got from POST
         SizePostDto sizePostDto = SizePostDto.builder()
                 .size("1000ML")
@@ -106,9 +107,89 @@ public class SizeServiceTest {
                 .size("1000ML")
                 .build();
 
+        // Act
         SizeFullDto returnedSizeFullDto = sizeService.saveSize(sizePostDto);
 
+        // Assert
         Assertions.assertEquals(expectedReturn.getSize(), returnedSizeFullDto.getSize());
     }
 
+    @Test
+    public void SizeService_UpdateSize_ReturnsSizeFullDtoSaved() {
+        // Arrange
+        SizeModel initialSizeModel = SizeModel.builder()
+                .size("1000ML")
+                .build();
+
+        Long sizeId = 1L;
+        SizePostDto recivedSizeFromPost = SizePostDto.builder()
+                .id(sizeId)
+                .size("1500ML")
+                .build();
+
+        SizeModel expectedSizeModel = SizeModel.builder()
+                .size("1500ML")
+                .build();
+
+        Mockito.when(sizeRepository.findById(sizeId))
+                .thenReturn(Optional.of(initialSizeModel));
+
+        Mockito.when(sizeRepository.save(Mockito.any(SizeModel.class)))
+                .thenReturn(expectedSizeModel);
+
+        // Act
+        SizeFullDto returnedSizeFullDto = sizeService.updateSize(sizeId, recivedSizeFromPost);
+
+        // Assert
+        Assertions.assertEquals(expectedSizeModel.getSize(), returnedSizeFullDto.getSize());
+        Mockito.verify(sizeRepository, Mockito.times(1)).findById(sizeId);
+        Mockito.verify(sizeRepository, Mockito.times(1)).save(Mockito.any(SizeModel.class));
+    }
+
+    @Test
+    public void SizeService_UpdateSize_ThrowsIncompatibleIdsException() {
+        // Arrange
+        Long sizeId = 2L;
+        SizePostDto recivedSizeFromPost = SizePostDto.builder()
+                .id(1L)
+                .size("1000ML")
+                .build();
+
+        // Act
+        // Assert
+        Assertions.assertThrows(IncompatibleIdsException.class, () -> {
+            sizeService.updateSize(sizeId, recivedSizeFromPost);
+        });
+    }
+
+    @Test
+    public void SizeService_DeleteSizeById_ReturnsVoid() {
+        // Arrange
+        Long sizeId = 1L;
+
+        Mockito.when(sizeRepository.existsById(sizeId))
+                .thenReturn(true);
+
+        // Act
+        sizeService.deleteSizeById(sizeId);
+
+        // Assert
+        Mockito.verify(sizeRepository, Mockito.times(1)).deleteById(sizeId);
+    }
+
+    @Test
+    public void SizeService_DeleteSizeById_ThrowsResourceNotFoundException() {
+        // Arrange
+        Long sizeId = 1L;
+
+        Mockito.when(sizeRepository.existsById(sizeId))
+                .thenReturn(false);
+
+        // Act
+        // Assert
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+           sizeService.deleteSizeById(sizeId);
+        });
+        Mockito.verify(sizeRepository, Mockito.times(0)).deleteById(sizeId);
+    }
 }
