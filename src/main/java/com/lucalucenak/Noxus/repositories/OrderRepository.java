4f2ea5d49f80f10303630d3dd1d2e4ebf84afd67
Lucas Lucena <lucas.lucenak@gmail.com>
2023-10-18 10:25:22 -0300

    Add exceptions

--- src/main/java/com/lucalucenak/Noxus/repositories/OrderRepository.java
+++ src/main/java/com/lucalucenak/Noxus/repositories/OrderRepository.java
@@ -4,6 +4,10 @@ import com.lucalucenak.Noxus.models.OrderModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<OrderModel, Long> {
+    boolean existsByDeliveryId(Long deliveryId);
+    Optional<OrderModel> findByDeliveryId(Long deliveryId);
 }
