7722f01066d49f86849bc4fecd3b0644b1fcbca4
Lucas Lucena <lucas.lucenak@gmail.com>
2023-10-18 11:36:05 -0300

    Add cellphoneNumber

--- src/main/java/com/lucalucenak/Noxus/dtos/post/ClientAccountPostDto.java
+++ src/main/java/com/lucalucenak/Noxus/dtos/post/ClientAccountPostDto.java
@@ -32,19 +32,33 @@ public class ClientAccountPostDto {
     @Email
     private String email;
 
+    @NotNull(message = "Field cellphoneNumber shouldn't be null")
+    @NotEmpty(message = "Field cellphoneNumber shouldn't be empty")
+    @NotBlank(message = "Field cellphoneNumber shouldn't be blank")
+    private String cellphoneNumber;
+
     @NotNull(message = "Field statusId shouldn't be null")
     private Long statusId;
 
     public ClientAccountPostDto() {
     }
 
-    public ClientAccountPostDto(Long id, String firstName, String lastName, String cpf, String email, Long statusId) {
+    public ClientAccountPostDto(Long id, String firstName, String lastName, String cpf, String email, String cellphoneNumber, Long statusId) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.cpf = cpf;
         this.email = email;
+        this.cellphoneNumber = cellphoneNumber;
         this.statusId = statusId;
+    }
+
+    public String getCellphoneNumber() {
+        return cellphoneNumber;
+    }
+
+    public void setCellphoneNumber(String cellphoneNumber) {
+        this.cellphoneNumber = cellphoneNumber;
     }
 
     public Long getId() {
