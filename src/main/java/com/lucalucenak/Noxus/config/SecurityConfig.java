47be5c9de3165e6d0657a7d47a2c8d57fc1bcf8d
Daniel Xavier <dxavier194@gmail.com>
2023-11-20 17:21:45 -0300

    Liberada a rota GET /product-types

--- src/main/java/com/lucalucenak/Noxus/config/SecurityConfig.java
+++ src/main/java/com/lucalucenak/Noxus/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
                 .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/order/find-by-client-account-id/{clientAccountId}")).hasRole("USER")
                 // Product Routes
                 .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/product")).permitAll()
+                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/product-type")).permitAll()
 
                 .anyRequest().hasRole("ADMIN")
         );
